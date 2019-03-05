d3a97d38395462259e4189af56c10e74fa74b25d
yellow3 <1309832228@qq.com>
2019-03-05 11:01:06 +0800

    nav-header init

--- src/pages/nav/components/nav-header.vue
+++ src/pages/nav/components/nav-header.vue
@@ -49,7 +49,7 @@
 
 <script>
 export default {
-  name: 'HomeHeader',
+  name: 'NavHeader',
   data () {
     return {
       show: false,
---
+++ src/pages/nav/nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+  </div>
+</template>
+
+<script>
+import NavHeader from './components/nav-header'
+export default {
+  name: 'Nav',
+  components: {
+    NavHeader
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+
+</style>
